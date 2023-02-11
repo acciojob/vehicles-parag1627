17a5a77d3323cc43970c1d43ab43ed8f10955be3
parag1627 <paragpatil973047@gmail.com>
2023-02-11 17:02:39 +0530

    i do

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,22 +4,3 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-class Boat implements WaterVehicle {
-    String name;
-    int capacity;
-
-    Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    @Override
-    public String getVehicleName() {
-        return this.name;
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return this.capacity;
-    }
-}
